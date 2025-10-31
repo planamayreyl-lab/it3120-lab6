4b651867a2afc9c99be067fc3c93986166b23c43
erekajordan2-ai <erekajordan2@gmail.com>
2025-10-31 15:34:49 +0800

    Update app title to 'WELCOME TO MY HOMEPAGE PAGE'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WELCOME TO MY PAGE',
+      title: 'WELCOME TO MY HOMEPAGE PAGE',
       theme: ThemeData(
         // This is the theme of your application.
         //
