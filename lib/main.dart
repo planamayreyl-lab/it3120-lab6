43e89a433dca48619d2ede3b45763f0fec52d0dd
gordonjamby123-spec <gordonjamby123@gmail.com>
2025-10-31 15:16:00 +0800

    Changing Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WELCOME TO MY WEBPAGE',
+      title: 'WELCOME TO MY PAGE',
       theme: ThemeData(
         // This is the theme of your application.
         //
